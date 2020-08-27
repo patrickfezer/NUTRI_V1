057cd9d524611b86f9ec2467bc6efbb98095a362
Patrick Fezer <p.fezer@gmx.de>
2020-08-27 16:04:55 +0200

    Initial Commit

---
+++ Kalorientracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kalorientracker
+//
+//  Created by Patrick Fezer on 27.08.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kalorientracker/KalorientrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KalorientrackerApp.swift
+//  Kalorientracker
+//
+//  Created by Patrick Fezer on 27.08.20.
+//
+
+import SwiftUI
+
+@main
+struct KalorientrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
